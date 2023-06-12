30b39f05b7f8c25d2c7cde4c5a2d2b0f626a5453
Eshaal Rahim <eshaal.rahim@gmail.com>
2023-06-12 14:05:43 -0400

    Initial Commit

---
+++ JustUs/JustUsApp.swift
@@ -0,0 +1,17 @@
+//
+//  JustUsApp.swift
+//  JustUs
+//
+//  Created by Eshaal Rahim on 6/12/23.
+//
+
+import SwiftUI
+
+@main
+struct JustUsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
